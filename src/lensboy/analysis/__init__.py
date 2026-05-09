9362794db6adef56222425dc18fb5f92fda346d8
Robertleoj <robert.leo.jonsson@gmail.com>
2026-05-09 18:29:10 +0200

    just error if deps are missing

--- src/lensboy/analysis/__init__.py
+++ src/lensboy/analysis/__init__.py
@@ -1,23 +1,5 @@
-from lensboy.analysis.unproject_lut import (
-    UnprojectLUTAccuracyReport,
-    UnprojectLUTErrorHeatmap,
-    UnprojectLUTSampleAccuracy,
-    compute_lut_error_heatmap,
-    estimate_lut_accuracy,
-    sample_lut_accuracy,
-)
-
-__all__ = [
-    "UnprojectLUTAccuracyReport",
-    "UnprojectLUTErrorHeatmap",
-    "UnprojectLUTSampleAccuracy",
-    "compute_lut_error_heatmap",
-    "estimate_lut_accuracy",
-    "sample_lut_accuracy",
-]
-
 try:
-    from lensboy.analysis.plots import (  # noqa: F401
+    from lensboy.analysis.plots import (
         draw_points,
         plot_detection_coverage,
         plot_distortion_grid,
@@ -25,16 +7,31 @@ try:
         plot_undistortion,
         plot_unproject_lut_error_heatmap,
     )
-except ImportError:
-    pass
-else:
-    __all__.extend(
-        [
-            "draw_points",
-            "plot_detection_coverage",
-            "plot_distortion_grid",
-            "plot_projection_diff",
-            "plot_unproject_lut_error_heatmap",
-            "plot_undistortion",
-        ]
+    from lensboy.analysis.unproject_lut import (
+        UnprojectLUTAccuracyReport,
+        UnprojectLUTErrorHeatmap,
+        UnprojectLUTSampleAccuracy,
+        compute_lut_error_heatmap,
+        estimate_lut_accuracy,
+        sample_lut_accuracy,
     )
+except ImportError as e:
+    raise ImportError(
+        "The analysis module requires extra dependencies. "
+        "Install them with: pip install lensboy[analysis]"
+    ) from e
+
+__all__ = [
+    "UnprojectLUTAccuracyReport",
+    "UnprojectLUTErrorHeatmap",
+    "UnprojectLUTSampleAccuracy",
+    "compute_lut_error_heatmap",
+    "draw_points",
+    "estimate_lut_accuracy",
+    "plot_detection_coverage",
+    "plot_distortion_grid",
+    "plot_projection_diff",
+    "plot_undistortion",
+    "plot_unproject_lut_error_heatmap",
+    "sample_lut_accuracy",
+]
